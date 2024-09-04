4ccc3e8402ab29cbc6fd417fdd57618ed22aa081
Oscar Andersson <anderssonoscar03@gmail.com>
2024-09-04 13:38:34 +0200

    Add memory manager functions

---
+++ memory_manager.c
@@ -0,0 +1,11 @@
+#include "memory_manager.h"
+
+void mem_init(size_t size) {}
+
+void *mem_alloc(size_t size) { return NULL; }
+
+void mem_free(void *block) {}
+
+void *mem_resize(void *block, size_t size) { return NULL; }
+
+void mem_deinit() {}
