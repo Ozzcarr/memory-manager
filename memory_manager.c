634ccc2d49c3f14640aade4569b0b9aef353085f
Oscar Andersson <anderssonoscar03@gmail.com>
2024-09-08 00:49:40 +0200

    Use ends instead of occupied for improved efficiency

--- memory_manager.c
+++ memory_manager.c
@@ -1,8 +1,8 @@
 #include "memory_manager.h"
 
 void *memory;
-unsigned char *occupied;
 unsigned char *starts;
+unsigned char *ends;
 size_t mem_size;
 
 /**
@@ -32,7 +32,7 @@ void clear_bit(unsigned char *array, size_t index) {
  * @param index The index of the bit to check.
  * @return True if the bit is set, false otherwise.
  */
-bool is_bit_set(const unsigned char *array, size_t index) {
+bool is_set(const unsigned char *array, size_t index) {
     return array[index / 8] & (1 << (index % 8));
 }
 
@@ -44,8 +44,8 @@ bool is_bit_set(const unsigned char *array, size_t index) {
 void mem_init(size_t size) {
     memory = malloc(size);
     size_t bit_array_size = (size + 7) / 8;
-    occupied = calloc(bit_array_size, 1);
     starts = calloc(bit_array_size, 1);
+    ends = calloc(bit_array_size, 1);
     mem_size = size;
 }
 
@@ -57,24 +57,21 @@ void mem_init(size_t size) {
  * allocation fails.
  */
 void *mem_alloc(size_t size) {
-    if (size == 0 || size > mem_size) {
-        return NULL;
-    }
+    if (size == 0 || size > mem_size) return NULL;
 
     // Loop to find a place to allocate (first-fit)
     int consecutive_free = 0;
+    bool occupied = false;
     for (size_t i = 0; i < mem_size; i++) {
-        consecutive_free =
-            (!is_bit_set(occupied, i)) ? consecutive_free + 1 : 0;
+        if (is_set(starts, i)) occupied = true;
+        consecutive_free = (!occupied) ? consecutive_free + 1 : 0;
         if (consecutive_free == size) {
             // Set memory as allocated
-            for (size_t j = 0; j < size; j++) {
-                set_bit(occupied, i - j);
-            }
             set_bit(starts, i - size + 1);
-
+            set_bit(ends, i);
             return memory + i - size + 1;
         }
+        if (is_set(ends, i)) occupied = false;
     }
 
     return NULL;
@@ -91,14 +88,12 @@ void mem_free(void *block) {
     // Get start index to free
     size_t index = block - memory;
     if (index >= mem_size) return;
-    if (!is_bit_set(starts, index)) return;
-    clear_bit(starts, index);
+    if (!is_set(starts, index)) return;
 
     // Set memory as no longer occupied
-    for (size_t i = index; i < mem_size; i++) {
-        if (is_bit_set(starts, i) || !is_bit_set(occupied, i)) break;
-        clear_bit(occupied, i);
-    }
+    clear_bit(starts, index);
+    while (!is_set(ends, index)) index++;
+    clear_bit(ends, index);
 }
 
 /**
@@ -113,41 +108,30 @@ void *mem_resize(void *block, size_t size) {
     if (size <= 0) {
         if (block) mem_free(block);
         return NULL;
-    };
+    }
     if (!block) return mem_alloc(size);
 
     // Get start index to resize
-    size_t index = block - memory;
-    if (index >= mem_size) return NULL;
+    size_t startIndex = block - memory;
+    if (startIndex >= mem_size) return NULL;
+    if (!is_set(starts, startIndex)) return NULL;
 
-    // Get current size
-    size_t current_size;
-    if (is_bit_set(starts, index)) {
-        current_size = 1;
-        for (size_t i = index + 1; i < mem_size; i++) {
-            if (is_bit_set(starts, i) || !is_bit_set(occupied, i)) break;
-            current_size++;
-        }
-    } else {
-        return NULL;
-    }
+    // Get end index
+    size_t endIndex = startIndex;
+    while (!is_set(ends, endIndex)) endIndex++;
 
+    size_t current_size = endIndex - startIndex + 1;
     int change = size - current_size;
 
     if (change > 0) {
         // Check if there is space to expand
-        size_t available = current_size;
-        for (size_t i = index + current_size; i < mem_size; i++) {
-            if (is_bit_set(starts, i) || is_bit_set(occupied, i)) break;
-            available++;
-        }
+        size_t stopIndex = endIndex;
+        while (stopIndex < mem_size && !is_set(starts, stopIndex)) stopIndex++;
 
-        if (available >= size) {
+        if (stopIndex - endIndex >= change) {
             // Expand
-            for (size_t i = index + current_size; i < index + size; i++) {
-                set_bit(occupied, i);
-            }
-
+            clear_bit(ends, endIndex);
+            set_bit(ends, endIndex + change);
             return block;
         } else {
             // Move memory block
@@ -159,9 +143,8 @@ void *mem_resize(void *block, size_t size) {
         }
     } else if (change < 0) {
         // Shrink
-        for (size_t i = index + size; i < index + current_size; i++) {
-            clear_bit(occupied, i);
-        }
+        clear_bit(ends, endIndex);
+        set_bit(ends, startIndex + size - 1);
         return block;
     }
 
@@ -174,7 +157,7 @@ void *mem_resize(void *block, size_t size) {
  */
 void mem_deinit() {
     free(memory);
-    free(occupied);
     free(starts);
+    free(ends);
     mem_size = 0;
 }
