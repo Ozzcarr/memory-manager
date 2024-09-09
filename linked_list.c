9d98caddd6971833a20947e21e96fbc44eef68ac
Oscar Andersson <anderssonoscar03@gmail.com>
2024-09-09 13:25:02 +0200

    Added base for linked list

---
+++ linked_list.c
@@ -0,0 +1,19 @@
+#include "linked_list.h"
+
+void list_init(Node **head) {}
+
+void list_insert(Node **head, int data) {}
+
+void list_insert_after(Node *prev_node, int data) {}
+
+void list_insert_before(Node **head, Node *next_node, int data) {}
+
+void list_delete(Node **head, int data) {}
+
+Node *list_search(Node **head, int data) { return NULL; }
+
+void list_display(Node **head, Node *start_node, Node *end_node) {}
+
+int list_count_nodes(Node **head) { return 0; }
+
+void list_cleanup(Node **head) {}
